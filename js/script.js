c5cf5fd4e937ce1f39608e3079ea0266511ee2ed
Mostyn Griffith (NewMAC) <mostyngriffith@mostyns-mbp-3.risd.edu>
2017-10-23 16:43:45 -0400

    05

--- js/script.js
+++ js/script.js
@@ -6,23 +6,24 @@ let lazy = lazyload();
     $("img.lazyload").lazyload();
 
 
+
+    $(".05").click(function(){
+        $(".content-05").toggle();
+    });
+
     $(".04").click(function(){
-        // $(this).slideToggle()
         $(".content-04").toggle();
     });
 
     $(".03").click(function(){
-        // $(this).slideToggle()
         $(".content-03").toggle();
     });
 
     $(".02").click(function(){
-        // $(this).slideToggle()
         $(".content-02").toggle();
     });
 
     $(".01").click(function(){
-        // $(this).slideToggle()
         $(".content-01").toggle();
     });
 
